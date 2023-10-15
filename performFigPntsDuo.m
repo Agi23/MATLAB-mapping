0add11dd7d1a9a78611fa5fb96bc083fdb4d527d
Agi23 <agoritsa.spirakis@gmail.com>
2023-10-15 15:06:26 +0200

    2 point cloud selection

---
+++ performFigPntsDuo.m
@@ -0,0 +1,63 @@
+function [pnts1, pnts2] = performFigPntsDuo(pc1, pc2)
+%UNTITLED4 Summary of this function goes here
+%   Detailed explanation goes here
+% f1=figure;
+% figure(f1);
+% pcshow(ptCloud, "AxesVisibility","on")
+% xlabel('X');
+% ylabel('Y');
+% zlabel('Z');
+% title("pc1")
+% datacursormode(f1, 'on');
+
+%x = linspace(0,10,150);
+%y = cos(5*x);
+%fig = figure;
+%plot(x,y,'Color',[0,0.7,0.9])
+%title('2-D Line Plot')
+%xlabel('x')
+%ylabel('cos(5x)')
+fig1=figure;
+pcshow(pc1, "AxesVisibility","on")
+xlabel('X');
+ylabel('Y');
+zlabel('Z');
+title("pc1")
+fig2=figure;
+pcshow(pc2, "AxesVisibility","on")
+xlabel('X');
+ylabel('Y');
+zlabel('Z');
+title("pc2")
+
+% Enable data cursor mode
+datacursormode on
+dcm_obj1 = datacursormode(fig1);
+dcm_obj2 = datacursormode(fig2);
+% Set update function
+%set(dcm_obj,'UpdateFcn',@myupdatefcn)
+% Wait while the user to click
+pnts1={};
+pnts2={};
+count1=0;
+count2=0;
+for k = 1:5
+    disp('Click line to display a data tip, then press "Return"')
+    pause 
+    % Export cursor to workspace
+    info_struct1 = getCursorInfo(dcm_obj1);
+    info_struct2 = getCursorInfo(dcm_obj2);
+    if isfield(info_struct1, 'Position')
+        count1=count1+1;
+        disp('Clicked positioin of 1 is')
+        disp(info_struct1.Position)
+        pnts1{count1}=info_struct1.Position;
+    elseif isfield(info_struct2, 'Position')
+        count2=count2+1;
+        disp('Clicked positioin of 2 is')
+        disp(info_struct2.Position)
+        pnts2{count2}=info_struct2.Position;
+    end
+end
+
+end
